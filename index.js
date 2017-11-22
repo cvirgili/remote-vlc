e736592891a376124bf1c980f1306945fa805f34
cvirgili <cristiano.virgili@gmail.com>
2017-11-22 21:04:58 +0100

    ok

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-var Remote = require('vlc-remote');
-var remote = new Remote(port = 45500, host = "127.0.0.1");
-
-remote.play("V:\\video-sync\\2.mp4", function(err, body) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(body);
+var net = require('net');
+var socket = net.Socket()
+socket.connect(8080, "10.64.120.91", function(conn) {
+    console.log('connected!');
+    var r = socket.write("Help\n");
+    socket.on('data', function(data) {
+        console.log('data: ' + data);
+    });
+    socket.on('error', function(data) {
+        console.log('error: ' + data);
+    });
 });
 
+/*
+var Remote = require('vlc-remote');
+var remote = new Remote(port = 8080, host = "10.64.120.91");
+
+// remote.play("/video-sync/2.mp4", function(err, body) {
+//     if (err) {
+//         console.log('ERR: ' + err);
+//     } else {
+//         console.log('body: ' + body);
+//     }
+// });
+
+remote.fullscreen();
+
+//remote.pause();
 
 // remote.stop(function(err, body) {
 
 // });
+*/
